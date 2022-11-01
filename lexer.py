ce31cac6351f436193b2bccf357e501b97263bf0
Kenny Rojas <18-10595@usb.ve>
2022-11-01 10:18:08 -0400

    Sera esta la version final?...

--- lexer.py
+++ lexer.py
@@ -1,5 +1,17 @@
-from src import AnalizadorLexicoGCL as lex
+from src.AnalizadorLexicoGCL import AnalizadorLexicoGCL
+import sys
 
-print ("abriendo el archivo test.gcl")
 
-lexer = lex.AnalizadorLexicoGCL("test.gcl")
+def main():
+    if(sys.argv[1] != None):
+        print ("Abriendo el archivo " + sys.argv[1])
+        lexer = AnalizadorLexicoGCL(sys.argv[1])
+        if lexer.respuesta["estatus"] == 0:
+            lexer.imprimirTokens()
+        elif lexer.respuesta["estatus"] ==1:
+            lexer.imprimirErrores()
+    else:
+        print('Introduzca un archivo valido')
+
+if __name__ == '__main__':
+    main()
--- src/AnalizadorLexicoGCL.py
+++ src/AnalizadorLexicoGCL.py
@@ -1,5 +1,6 @@
 import ply.lex as lex
 import re
+
 class AnalizadorLexicoGCL:
     """
     Al instanciarse la clase se ejecuta el 
@@ -8,15 +9,28 @@ class AnalizadorLexicoGCL:
     en el atributo respuesta como un diccionario
     """
     def __init__(self, nombre_archivo):
-        self.archivo = open(nombre_archivo)
-        
         self.respuesta = {
-            'estatus' : -1,
+            'estatus' : 0,
             'errores': [],
             'resultado': '',
         }
-        self.lexer()
-        self.archivo.close()
+        
+        self.tokens = []
+        try:
+            if nombre_archivo[-4:len(nombre_archivo)] != ".gcl":
+                self.respuesta['estatus'] = 3
+                self.respuesta['resultado'] = "Incorrect extension"
+                return
+
+            self.archivo = open(nombre_archivo)
+        
+            lexer = self.lexer()
+            self.analizarArchivo(lexer)
+            self.archivo.close()
+
+        except FileNotFoundError:
+            self.respuesta['estatus'] = 2
+            self.respuesta['resultado'] = "File not found"
     
     """
     Analizador lexico.
@@ -37,7 +51,10 @@ class AnalizadorLexicoGCL:
             'skip' : 'TkSkip',
             'print' : 'TkPrint',
             'true' : 'TkTrue',
-            'false' : 'TkFalse'
+            'false' : 'TkFalse',
+            'int' : 'TkInt',
+            'array' : 'TkArray',
+            'bool' : 'TkBool'
         }
 
         tokens = [
@@ -56,7 +73,7 @@ class AnalizadorLexicoGCL:
             'TkAsig',
             'TkSemicolon',
             'TkArrow',
-            'TkGuard', # Se agrego hace poco al enunciado de la etapa 1
+            'TkGuard',
 
             # Tokens de Simbolos de operadores
             'TkPlus',
@@ -84,13 +101,8 @@ class AnalizadorLexicoGCL:
 
         tokens = tokens + list(reservadas.values())
 
-        # # Definicion de las expresiones regulares de los tokens
-
-        t_TkNum = r'\d' # No estoy seguro si seria asi 
-        #t_TkString = r'' # no hay que detectar strings (?)
-        t_TkTrue = r'true'
-        t_TkFalse = r'false'
-
+        # Definicion de las expresiones regulares de los tokens
+        t_TkString = r"[\'\"].+[\'\"]" 
         t_TkOBlock = r'\|\['
         t_TkCBlock = r'\]\|'
         t_TkSoForth = r'\.\.'
@@ -112,78 +124,125 @@ class AnalizadorLexicoGCL:
         t_TkLeq = r'<='
         t_TkGeq = r'>='
         t_TkGreater = r'>'
-        t_TkEqual = r'='
+        t_TkEqual = r'=='
         t_TkNEqual = r'!='
         t_TkOBracket = r'\['
         t_TkCBracket = r'\]'
         t_TkTwoPoints = r':'
         t_TkConcat = r'\.'
 
-        t_ignore_TkComment = r'//'
-        t_ignore_TkNewLine = r'\n'
-        #t_ignore_TkSpace = r' '
-
-        # podemos ahorrarnos todos los ignore anteriores si hacemos
-        # T_ignore = r '//| |\n'
-
-        #Definicion de reglas sobre los tokens
+        t_ignore_TkComment = r'//.*'
         
-        # regla para verificar si el Id no es una palabra reservada
-        def t_TkId(t): # por algun motivo extraño no funciona bien
-            r'[a-zA-z_][a-zA-z_0-9]*'
+        # Kenny: ignora uno o mas saltos de lineas
+        #        ademas en windows por algun motivo los saltos 
+        #        de linea no son solo \n si no que es \r\n
+        #        fuck windows >:V
+        t_ignore_TkNewLine = r'\n+|\r\n+'
+        
+        # Kenny: Version fancy para ignorar espacios y tabulaciones
+        t_ignore_TkSpace = r'[ \t\r\n]+'
+
+        # Definicion de reglas sobre los tokens
+        
+        # Detecta los strings que tenga solo digitos y lo convierte en int's
+        # Kenny: Creo que puede ser util pasarlo a int asi que lo agregue
+        def t_TkNum(t):
+            r'\d+'
+            t.value = int(t.value)
+            return t
+                          
+        # Regla para verificar si el Id no es una palabra reservada
+        def t_TkId(t):
+            r'[a-zA-Z_][a-zA-Z_0-9]*'
             t.type = reservadas.get(t.value, 'TkId')
             return t
 
         # manejador de errores del lexer
+        # solo hace que el lexer ignore el error, el manejo mas
+        # detallado del error se hace mas abajo
         def t_error(e):
-            print("Error con el caracter '%s'" % e.value[0])
             e.lexer.skip(1)
+            return e
 
         
         # Inicializacion del lexer
         analizador_lexer = lex.lex()
+        return analizador_lexer
+
+    def analizarArchivo(self, lexer):
+        # Kenny: AnalizarArchivo MK2, devolvi la parte de respuesta por que 
+        #        me parecio super util al menos lo de status, usando eso y la lista
+        #        de errores pude terminar todo lo que hay que imprimir
+        #        P.D: aun tengo ver si funciona bien los errores con los caracteres
+        #        escapados
         
-        # solo para comprobar la salida
+        # Se almacenan los Tokens
+
+        linea = 1
+
         for line in self.archivo:
-            analizador_lexer.input(line)
+            lexer.input(line)            
             while True:
-                token = analizador_lexer.token()
+                token = lexer.token()
                 if not token:
                     break
-                else:
-                    print(token)
+                elif token.type == 'error':
+                    if  self.respuesta['estatus'] != 1: 
+                        self.respuesta['estatus'] = 1
+                        self.respuesta['resultado'] = 'Syntaxis Error'
+                    self.respuesta['errores'].append(f"Error: Unexpected character \"{token.value[0]}\" in row {linea}, column {token.lexpos+1}")
+                    self.tokens = []
+                elif self.respuesta['estatus'] == 0:
+                    if token.type == "TkString" and self.analizarString(token,linea)== False:
+                        self.respuesta['estatus'] = 1
+                        self.respuesta['resultado'] = 'Syntaxis Error'
+                        self.tokens = []
+                    self.tokens.append(self.formatearToken(token,linea))
+            
+            linea += 1  
 
+    # Detecta los strings, si tiene caracteres escapados \. los analiza 
+    # si el caracter no es uno de los permitidos ignoramos el string por
+    # lo que dara error en las comillas y la barra invertida 
+    # devuelve True si todo fue bien
+    # Devuelve false si hay un error y añade los errores a la lista respuesta["errores"]
+    def analizarString(self, t,lineno) :
+        caracteresEscapados = re.findall(r"\\.",t.value)
+        error = False
+        for cha in caracteresEscapados:
+            if cha != "\\n" and cha != "\\\\" and cha!="\\":
+                error = True
+                break
+        if error == True: 
+            self.respuesta['errores'].append(f"Error: Unexpected character \"{t.value[0]}\" in row {lineno}, column {t.lexpos+1}")
+            caracteresEscapados = re.finditer(r"\\.",t.value)
+            for cha in caracteresEscapados:
+                self.respuesta['errores'].append(f"Error: Unexpected character \"{t.value[cha.start()]}\" in row {lineno}, column {cha.start()+t.lexpos+1}")
+            
+            self.respuesta['errores'].append(f"Error: Unexpected character \"{t.value[-1]}\" in row {lineno}, column {t.lexpos+len(t.value)}")
+            
+        return not error
+    
+    def obtenerTokens(self):
+        return self.tokens
 
-    """
-    Lista de estatus de la respuesta:
-    0: Analisis Exitoso
-        Errores es una lista vacia
-        Resultado contiene el string del analisis
-        
-    1: Analisis Fallido por error en sintaxis
-        Errores Almacenados en respuesta['errores']
-        Resultado indica que exite error de sintaxis
-
-    2: Archivo no encontrado
-        Errores es una lista vacia
-        Resultado indica que no se encontro el archivo
-
-    3: Archivo no admitido (Extension invalida)
-        Errores es una lista vacia
-        Resultado indica que el archivo no es valido
-    """
-    def obtenerResultado(self) -> str:
-        pass
-    def obtenerEstatus(self) -> int:
-        pass
-
+    def imprimirTokens(self):
+        for token in self.tokens:
+            print(token)
     
     def obtenerErrores(self) -> tuple:
-        errores_str = ''
+        errores_str = '\n'.join(self.respuesta['errores'])
         # Se retorna una tupla de string de errores imprimible y una 
         # lista de strings de errores
         return ( errores_str, self.respuesta['errores'] )
+    
+    def imprimirErrores(self):
+        print(self.obtenerErrores()[0])
 
-
-
-
+    # Dado un token y su linea en el archivo se almacena el
+    # token en el formato solicitado
+    def formatearToken(self, token, linea) -> str:
+        argumentable = [ 'TkId', 'TkString', 'TkNum' ]
+        if (token.type in argumentable):
+            return '{}(\'{}\') {} {}'.format(token.type, token.value, linea, token.lexpos+1)
+        return str(token.type) + ' ' +str(linea) + ' '+str(token.lexpos+1)
