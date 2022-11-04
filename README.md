8615e91091a5f9849f6bdd1cdda607ae64971639
Eros Cedeño <16-10216@usb.ve>
2022-11-04 18:58:57 -0400

    Documentando lexer.py

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 El presente es un analizador lexico del lenguaje teorico diseñado por Edsger Dijkstra para semantica de transformación de predicados GCL.
 
 Proyecto para la asignatura CI3725 - Traductores e interpretadores de la Universidad Simón Bolivar de Venezuela. Profesor a cargo del curso: Federico Flaviani. Trimestre Sep Dic 2022.
+
 ### Funcionamiento
 El traductor recibe un programa escrito en GCL y devuelve su semántica. Al recibir un programa con extensión .gcl como argumento, devuelve la relación correspondiente, escrita con los símbolos de la teoría de conjuntos.
+
 ### Requerimientos
 - Interprete de Python3
 - Libreria ply-3.11
--- lexer.py
+++ lexer.py
@@ -1,10 +1,32 @@
+"""
+El presente es el cliente del analizador lexico de GCL
+
+Al ejecutarse es necesario pasarle como argumento el nombre
+del archivo a leer. Es condicion necesaria que la extension
+del archivo sea .gcl y que tenga permisos de lectura.
+
+Al ejecutarse el cliente, este instancia la clase AnalizadorLexicoGCL
+e imprime el resultado del analisis
+"""
+
 from src.AnalizadorLexicoGCL import AnalizadorLexicoGCL
 import sys
 
 
 def main():
+    
+    # Si no se ingresa el nombre de un archivo por 
+    # los argumentos de ejecucion entonces se indicara el error
+    # Por otro lado si se indica correctamente el archivo
+    # entonces se instancia el analizador
     if(sys.argv[1] != None):
         lexer = AnalizadorLexicoGCL(sys.argv[1])
+
+        # El lexer muestra guarda en su diccionario
+        # respuesta el estatus del analisis si el resultado 
+        # fue exitoso (estatus igual a 1)
+        #  entonces se imprimen los tokens, si por otro lado el analisis
+        # falla (estatus == 1) se imprimen los errores del analisis
         if lexer.respuesta["estatus"] == 0:
             lexer.imprimirTokens()
         elif lexer.respuesta["estatus"] ==1:
@@ -12,5 +34,7 @@ def main():
     else:
         print('Introduzca un archivo valido')
 
+# En caso de utilizar este cliente desde otro archivo
+# este no ejecutara la funcion main automaticamente.
 if __name__ == '__main__':
     main()
